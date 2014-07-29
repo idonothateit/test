ec1513cd93e31b1036c40a24022ffce1136442a3
admin <saito@showtime.co.jp>
2014-07-29 19:31:13 +0900

    apache initial commit

---
+++ apache/recipes/default.rb
@@ -0,0 +1,13 @@
+﻿package "httpd" do
+   action :upgrade
+   version "2.2.27"
+end
+
+file "/var/www/html/index.html" do 
+   content "OK"
+   owner "apache"
+   group "apache"
+   mode 00755
+   action :create
+end
+
