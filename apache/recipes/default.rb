2b9e23142bb6f8b1c5e605c0edea45de23a5ac40
admin <saito@showtime.co.jp>
2014-07-29 19:52:35 +0900

    change from http22 to httpd24

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -1,13 +1,22 @@
 ﻿package "httpd" do
-   action :upgrade
-   version "2.2.27"
+  action :remove
+end
+
+package "httpd24" do
+  action :install
+  version "2.4.9"
+end
+
+service "httpd" do
+  action [ :enable, :start ]
+  supports :status => true, :restart => true, :reload => true
 end
 
 file "/var/www/html/index.html" do 
-   content "OK"
-   owner "apache"
-   group "apache"
-   mode 00755
-   action :create
+  content "OK"
+  owner "apache"
+  group "apache"
+  mode 00755
+  action :create
 end
 
